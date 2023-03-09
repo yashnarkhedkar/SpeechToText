244ae96f8d968c2c4ddfa4d9940acd2dfeecec6e
Shreeshail.Santosh.Yarandole <85671136+Shri25001@users.noreply.github.com>
2023-03-09 21:17:25 +0530

    shreeshail

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import Home from './pages/Home'
 import About from "./pages/About"
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
-import { useGoogleLogin, googleLogout  } from '@react-oauth/google';
+import { useGoogleLogin, googleLogout } from '@react-oauth/google';
 import axios from "axios"
 
 const App = () => {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
-  
+
   const handleLogout = () => {
     googleLogout();
     setUser(null);
@@ -24,7 +24,7 @@ const App = () => {
           }
         })
         setUser(res);
-        console.log(user)
+        console.log(res)
         navigate('/Home');
       } catch (err) {
         console.log(err)
@@ -40,15 +40,13 @@ const App = () => {
             <div className="flex space-x-4">
               <div>
                 <Link to="/" className="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900">
-                  <svg className="h-6 w-6 mr-1 text-blue-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
-                  </svg>
+                  <img src='/lp.png' className='w-10 h-10' />
                   <span className="font-bold">Company Name</span>
                 </Link>
               </div>
 
               <div className="flex items-center space-x-1">
-                <Link to="/home" className="py-5 px-3 text-gray-700 hover:text-gray-900">Home</Link>
+                <Link to="/home" className="hover:underline py-5 px-3 text-gray-700 hover:text-gray-900">Home</Link>
                 <Link to="/about" className="py-5 px-3 text-gray-700 hover:text-gray-900">About</Link>
               </div>
             </div>
@@ -56,20 +54,17 @@ const App = () => {
             <div className="flex items-center space-x-1">
               {user ?
                 <div className='flex space-x-4 items-center'>
-                  <h2 className='text-sm font-semibold'>{user.data.given_name}</h2>
+                  <h2 className='text-sm font-semibold'>{user.data.email}</h2>
                   <img className="w-10 h-10 cursor-pointer rounded-full" src={user.data.picture} alt="Rounded avatar" />
-                  <svg onClick={handleLogout} className="h-5 w-5 text-black cursor-pointer" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />  <path d="M7 12h14l-3 -3m0 6l3 -3" /></svg>
+                  <svg onClick={handleLogout} className="h-5 w-5 text-black cursor-pointer" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />  <path d="M7 12h14l-3 -3m0 6l3 -3" /></svg>
                 </div>
                 :
-                <a onClick={login} className="cursor-pointer relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group">
-                  <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
-                  <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-                    <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                  </span>
-                  <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-                    <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                  </span>
-                  <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">Get Started</span>
+                <a onClick={login}  className="relative px-5 py-2 font-medium text-white group">
+                  <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-indigo-500 group-hover:bg-indigo-700 group-hover:skew-x-12"></span>
+                  <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-indigo-700 group-hover:bg-indigo-500 group-hover:-skew-x-12"></span>
+                  <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-indigo-600 -rotate-12"></span>
+                  <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-indigo-400 -rotate-12"></span>
+                  <span className="relative">Get started</span>
                 </a>
               }
             </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,15 +4,25 @@ const About = () => {
 
   return (
     <>
-      <div className='my-6 p-8 flex w-full flex-col md:flex-row md:justify-between'>
+      <div className='bg-white my-6 p-8 flex w-full flex-col md:flex-row md:justify-between'>
         <div className='max-w-max font-extrabold text-5xl mt-5'>
-          <p>fadsssssssssfadfs</p>
-          <p>dasdasdasdada </p>
-          <p>in <span className='text-[#2f21f3]'>lorem</span></p>
+          
+          <h1 className="m-5 ml-1 mb-25  text-4xl text-black-100 md:text-6xl">
+            Hi, I&apos;m <br className="block md:hidden" />
+            <span className="relative">
+              <span className="text-indigo-600 h-20 pt-2 overflow-x-hidden whitespace-nowrap text-brand-accent">
+                Assistant <span className="text-3xl md:text-5xl">👋</span>
+              </span>
+              <span
+                className="{`${styles.cursor} absolute -bottom-0 left-0 -top-1 inline-block bg-white w-full animate-type will-change`}"
+              ></span>
+            </span>
+          </h1>
+          
           <p className='mt-8 font-normal xl:text-xl text-sm text-gray-400'>Create and collaborate on online documents in real-time and from any device.</p>
         </div>
         <div>
-          {/* <img src="loginImg.png" className='max-w-xs mt-4 md:mt-0 md:max-w-2xl' /> */}
+          {<img src="lp.png" className=' h-96 transform -scale-x-100 mr-16 max-w-xs mt-4 md:mt-0 md:max-w-2xl' />}
         </div>
       </div>
     </>
